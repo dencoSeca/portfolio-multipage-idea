a0cba5d0247dfe85e929a584eb13de75e694a0c4
Leon Brown <leon.brown71@gmail.com>
2021-04-23 14:03:35 +0100

    add work page, add text--lg class

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // Components
 import Header from '../components/Header'
 import Menu from '../components/Menu'
 
-function Layout({ children }) {
+function Layout({ children, location }) {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  useEffect(() => {
+    setMenuIsOpen(false)
+  }, [location])
 
   return (
     <>
---
+++ src/pages/work.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <div className="work">
+      <div className="work__statement">
+        <span className="work__statement-line text--lg with-color-block--grey">
+          I Like to create beautiful,
+        </span>
+        <span className="work__statement-line text--lg with-color-block--grey">
+          intuitive and responsive
+        </span>
+        <span className="work__statement-line text--lg with-color-block--grey">
+          stuffs on the web
+        </span>
+      </div>
+      <div className="work__projects"></div>
+    </div>
+  )
+}
+
+export default Work
