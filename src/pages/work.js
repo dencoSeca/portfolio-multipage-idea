ea4b8543a9265f707b9c76f7b96c1ffad69bcf25
Leon Brown <leon.brown71@gmail.com>
2021-04-26 15:07:30 +0100

    style work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,7 +3,6 @@ import { graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
-
 function Work({ data }) {
   const projects = data.allContentfulProject.nodes
   const scrollContainerRef = useRef(null)
@@ -21,21 +20,27 @@ function Work({ data }) {
         data-scroll-container
         ref={scrollContainerRef}
       >
-        <div className="statement">
+        <div className="statement__wrapper">
           <div className="statement__line text--lg">
             I like to make beautiful,
           </div>
           <div className="statement__line text--lg">
             intuitive and responsive
           </div>
-          <div className="statement__line text--lg">
-            stuffs on the web.
-          </div>
+          <div className="statement__line text--lg">stuffs on the web.</div>
         </div>
         <div className="projects">
           {projects.map((project, i) => (
-            <div className="project" key={i}>
-              <StaticImage src="../images/cherry-pie.jpg" alt="cherry pie" />
+            <div className="project__wrapper" key={i}>
+              <div className="project__text">
+                <h3 className="project__title text--md">{project.title}</h3>
+                <p className="project__tagline">Marketing website for Bragazzi's cafe and delicatessen.</p>
+              </div>
+              <StaticImage
+                className="project__image"
+                src="../images/cherry-pie.jpg"
+                alt="cherry pie"
+              />
             </div>
           ))}
         </div>
