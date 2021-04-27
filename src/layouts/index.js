fd0ef5c75da8b2dd787ec9c0df978537684b0e83
Leon Brown <leon.brown71@gmail.com>
2021-04-27 08:18:10 +0100

    move LocomotiveScrollProvider to layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 // Components
 import Header from '../components/Header'
@@ -6,17 +7,25 @@ import Menu from '../components/Menu'
 
 function Layout({ children, location }) {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const scrollContainerRef = useRef(null)
 
   useEffect(() => {
     setMenuIsOpen(false)
   }, [location])
 
   return (
-    <>
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+      }}
+      containerRef={scrollContainerRef}
+    >
       {menuIsOpen && <Menu />}
       <Header menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
-      {children}
-    </>
+      <main data-scroll-container ref={scrollContainerRef}>
+        {children}
+      </main>
+    </LocomotiveScrollProvider>
   )
 }
 
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,83 +1,70 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 function Work({ data }) {
   const projects = data.allContentfulProject.nodes
-  const scrollContainerRef = useRef(null)
 
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-      }}
-      containerRef={scrollContainerRef}
-    >
-      <div
-        className="work-screen"
-        data-scroll-container
-        ref={scrollContainerRef}
-      >
-        <div className="statement__wrapper" data-scroll-section>
-          <div
-            className="statement__line text--lg"
-            data-scroll
-            data-scroll-speed="2.5"
-            data-scroll-position="top"
-          >
-            I like to make beautiful,
-          </div>
-          <div
-            className="statement__line text--lg"
-            data-scroll
-            data-scroll-speed="1.75"
-            data-scroll-position="top"
-          >
-            intuitive and responsive
-          </div>
-          <div
-            className="statement__line text--lg"
-            data-scroll
-            data-scroll-speed="1"
-            data-scroll-position="top"
-          >
-            stuffs on the web.
-          </div>
+    <div className="work-screen">
+      <div className="statement__wrapper" data-scroll-section>
+        <div
+          className="statement__line text--lg"
+          data-scroll
+          data-scroll-speed="2.5"
+          data-scroll-position="top"
+        >
+          I like to make beautiful,
         </div>
-        <div className="projects">
-          {projects.map((project, i) => (
-            <div
-              className="project__wrapper"
-              id={`sticky${i}`}
-              key={i}
-              data-scroll-section
-            >
-              <div
-                className="project__text"
-                data-scroll
-                data-scroll-sticky
-                data-scroll-target={`#sticky${i}`}
-              >
-                <h3 className="project__title text--md text--italic with-color-block--grey">
-                  {project.title}
-                </h3>
-                <p className="project__tagline">
-                  {project.shortDescription.shortDescription}
-                </p>
-              </div>
-              <StaticImage
-                className="project__image"
-                src="../images/cherry-pie.jpg"
-                alt="cherry pie"
-                data-scroll
-                data-scroll-speed="-3"
-              />
-            </div>
-          ))}
+        <div
+          className="statement__line text--lg"
+          data-scroll
+          data-scroll-speed="1.75"
+          data-scroll-position="top"
+        >
+          intuitive and responsive
+        </div>
+        <div
+          className="statement__line text--lg"
+          data-scroll
+          data-scroll-speed="1"
+          data-scroll-position="top"
+        >
+          stuffs on the web.
         </div>
       </div>
-    </LocomotiveScrollProvider>
+      <div className="projects">
+        {projects.map((project, i) => (
+          <div
+            className="project__wrapper"
+            id={`sticky${i}`}
+            key={i}
+            data-scroll-section
+          >
+            <div
+              className="project__text"
+              data-scroll
+              data-scroll-sticky
+              data-scroll-target={`#sticky${i}`}
+            >
+              <h3 className="project__title text--md text--italic with-color-block--grey">
+                {project.title}
+              </h3>
+              <p className="project__tagline">
+                {project.shortDescription.shortDescription}
+              </p>
+            </div>
+            <StaticImage
+              className="project__image"
+              src="../images/cherry-pie.jpg"
+              alt="cherry pie"
+              data-scroll
+              data-scroll-speed="-3"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
