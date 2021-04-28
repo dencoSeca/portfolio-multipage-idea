ecbb379a498988d3f3f92d2abf90cd3a66a663fd
Leon Brown <leon.brown71@gmail.com>
2021-04-28 19:52:09 +0100

    install framer-motion and animate work statement component mount

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,85 +2,127 @@ import React, { useRef } from 'react'
 import { graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { AnimatePresence, motion } from 'framer-motion'
+
+// Animations
+const statementWrapperVariants = {
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.3,
+    },
+  },
+}
+
+const statementVariants = {
+  initial: {
+    opacity: 0,
+    x: '-10vw',
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      ease: [0.25, 1, 0.5, 1],
+    },
+  },
+}
 
 function Work({ data }) {
   const projects = data.allContentfulProject.nodes
   const scrollContainerRef = useRef(null)
 
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        direction: 'horizontal',
-        scrollFromAnywhere: true,
-        multipler: 0.75,
-      }}
-      containerRef={scrollContainerRef}
-    >
-      <div
-        className="work-screen"
-        data-scroll-container
-        ref={scrollContainerRef}
+    <AnimatePresence>
+      <LocomotiveScrollProvider
+        options={{
+          smooth: true,
+          direction: 'horizontal',
+          scrollFromAnywhere: true,
+          multipler: 0.75,
+          lerp: 0.1,
+        }}
+        containerRef={scrollContainerRef}
       >
-        <div className="scroll-section-wrapper" data-scroll-section>
-          <h2 className="statement">
-            <span
-              className="statement__line text--lg"
-              data-scroll
-              data-scroll-speed="4"
-              data-scroll-position="left"
+        <div
+          className="work-screen"
+          data-scroll-container
+          ref={scrollContainerRef}
+        >
+          <div className="scroll-section-wrapper" data-scroll-section>
+            <motion.h2
+              className="statement"
+              initial="initial"
+              animate="animate"
+              variants={statementWrapperVariants}
             >
-              I like to create beautiful,
-            </span>
-            <span
-              className="statement__line text--lg"
-              data-scroll
-              data-scroll-speed="2.5"
-              data-scroll-position="left"
-            >
-              intuitive and responsive
-            </span>
-            <span
-              className="statement__line text--lg"
-              data-scroll
-              data-scroll-speed="3"
-              data-scroll-position="left"
-            >
-              stuffs on the web.
-            </span>
-          </h2>
-          {projects.map((project, i) => (
-            <div className="project" key={i} data-scroll data-scroll-speed="1">
-              <div className="project__image">
-                <GatsbyImage
-                  image={project.image.gatsbyImageData}
-                  alt={project.image.description}
-                />
-                <div
-                  className="project__image-cover"
+              <motion.span
+                className="statement__line text--lg"
+                variants={statementVariants}
+                data-scroll
+                data-scroll-speed="4"
+                data-scroll-position="left"
+              >
+                I like to create beautiful,
+              </motion.span>
+              <motion.span
+                className="statement__line text--lg"
+                variants={statementVariants}
+                data-scroll
+                data-scroll-speed="2.5"
+                data-scroll-position="left"
+              >
+                intuitive and responsive
+              </motion.span>
+              <motion.span
+                className="statement__line text--lg"
+                variants={statementVariants}
+                data-scroll
+                data-scroll-speed="3"
+                data-scroll-position="left"
+              >
+                stuffs on the web.
+              </motion.span>
+            </motion.h2>
+            {projects.map((project, i) => (
+              <div
+                className="project"
+                key={i}
+                data-scroll
+                data-scroll-speed="1"
+              >
+                <div className="project__image">
+                  <GatsbyImage
+                    image={project.image.gatsbyImageData}
+                    alt={project.image.description}
+                  />
+                  <div
+                    className="project__image-cover"
+                    data-scroll
+                    data-scroll-offset="10%"
+                  ></div>
+                </div>
+                <h3
+                  className="project__title text--md"
                   data-scroll
                   data-scroll-offset="10%"
-                ></div>
+                >
+                  {project.title}
+                </h3>
+                <p
+                  className="project__short-description text--sm"
+                  data-scroll
+                  data-scroll-offset="10%"
+                >
+                  {project.shortDescription.shortDescription}
+                </p>
               </div>
-              <h3
-                className="project__title text--md"
-                data-scroll
-                data-scroll-offset="10%"
-              >
-                {project.title}
-              </h3>
-              <p
-                className="project__short-description text--sm"
-                data-scroll
-                data-scroll-offset="10%"
-              >
-                {project.shortDescription.shortDescription}
-              </p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </LocomotiveScrollProvider>
+      </LocomotiveScrollProvider>
+    </AnimatePresence>
   )
 }
 
